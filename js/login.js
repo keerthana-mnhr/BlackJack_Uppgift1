e99f3a0afa2ea77ebc57338a384ac943a828a75a
Keerthana Manoharan <keerthana.mnhr@gmail.com>
2026-05-14 00:55:25 +0200

    Made register page , and redirection to game page after succesfull login

--- js/login.js
+++ js/login.js
@@ -6,10 +6,21 @@ function login(){
 
     const password= document.getElementById("password").value;
 
-    if(username === "keerthana" && password === "12345"){
-        document.getElementById("message").innerText = "Login Successfull";
-    }
-    else{
-        document.getElementById("message").innerText="Login Failed";
-    }
+   
+    const users =JSON.parse(localStorage.getItem("users")) || [];
+
+    const registeredUser = users.find(user => user.username === username && user.password === password);
+
+    if(registeredUser){ 
+        document.getElementById("message").innerText = "Login successful. Redirecting to game...";
+        setTimeout(() => {
+            localStorage.setItem("currentUser", JSON.stringify(registeredUser));
+            window.location.href = "blackjack.html";
+        }, 1000);
+    } 
+    
+    else {
+    document.getElementById("message").innerText = "Invalid username or password";
+}
+
 }
--- js/registerUser.js
+++ js/registerUser.js
@@ -3,6 +3,50 @@ function registerUser() {
 
     const username= document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirm-password").value;
 
-    
-}
+    if(username === "" || password === "" || confirmPassword === ""){
+
+        document.getElementById("message").innerText = "Please fill in all the fields";
+
+        return;
+    }
+
+    if(password !== confirmPassword){
+
+        document.getElementById("message").innerText = "Passwords do not match";
+
+        return; 
+    }
+
+        let users= JSON.parse(localStorage.getItem("users")) || [];
+
+        const userExists = users.find(user =>user.username === username);
+
+        if(userExists){
+
+            document.getElementById("message").innerText = "Username already exists";
+            return;
+        }
+
+        const newUser = {
+
+            username: username,
+            password: password,
+            score : 0
+        };
+
+        users.push(newUser);
+        localStorage.setItem("users", JSON.stringify(users));
+
+       
+         
+        document.getElementById("message").innerText = "Registration successful. Please log in.";
+       
+            setTimeout(() => {localStorage.setItem("currentUser", JSON.stringify(newUser));   
+                 window.location.href="index.html";
+            },1.5000);
+
+
+       
+    }
